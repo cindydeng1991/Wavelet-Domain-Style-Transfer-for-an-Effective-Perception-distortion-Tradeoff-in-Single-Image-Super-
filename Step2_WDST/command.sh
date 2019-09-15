22bc58b1c2ab3d7eb4c3abda9b95c2d28d498d8c
cindydeng1991 <dengxin19910103@163.com>
2019-09-15 18:40:59 +0100

    Add files via upload

---
+++ Step2_WDST/command.sh
@@ -0,0 +1,6 @@
+python run_main.py --content Content/101_content_gv_level1.png --style Style/101_style_gv_level1.png --content_layers conv2_2   --max_size 584 --num_iter 5000 --output 101_gv_level1.png
+python run_main.py --content Content/101_content_gh_level1.png --style Style/101_style_gh_level1.png --content_layers conv2_2   --max_size 584 --num_iter 5000 --output 101_gh_level1.png
+python run_main.py --content Content/101_content_gd_level1.png --style Style/101_style_gd_level1.png --content_layers conv2_2   --max_size 584 --num_iter 5000 --output 101_gd_level1.png
+python run_main.py --content Content/101_content_gv_level2.png --style Style/101_style_gv_level2.png --content_layers conv2_2   --max_size 584 --num_iter 1000 --output 101_gv_level2.png
+python run_main.py --content Content/101_content_gh_level2.png --style Style/101_style_gh_level2.png --content_layers conv2_2   --max_size 584 --num_iter 1000 --output 101_gh_level2.png
+python run_main.py --content Content/101_content_gd_level2.png --style Style/101_style_gd_level2.png --content_layers conv2_2   --max_size 584 --num_iter 1000 --output 101_gd_level2.png
